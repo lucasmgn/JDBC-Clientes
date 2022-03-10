4f4eba3136ef7b6af8e2c098de52ac22060f2908
Lucas Magno <lucasmagno695@gmail.com>
2022-03-10 20:29:09 -0300

    Update JdbcDAOFactory.java

--- src/com/estudoJava/curso/dao/jdbc/JdbcDAOFactory.java
+++ src/com/estudoJava/curso/dao/jdbc/JdbcDAOFactory.java
@@ -8,16 +8,16 @@ import com.estudoJava.curso.dao.DAOFactory;
 
 public class JdbcDAOFactory extends DAOFactory {
 	
-	private Connection connection; //criando uma conex„o com o sql
+	private Connection connection; //criando uma conex√£o com o sql
 	
 	public JdbcDAOFactory() {
 		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");			//String de conex„o				//nome de usuario	//senha
-			this.connection = DriverManager.getConnection("jdbc:mysql://localhost/cadastro_cliente", "root", "auhpuk260202");
+			Class.forName("com.mysql.cj.jdbc.Driver");			//String de conex√£o				//nome de usuario	//senha
+			this.connection = DriverManager.getConnection("jdbc:mysql://localhost/cadastro_cliente", "root", "senha-do-bd");
 			
 			
 		}catch(Exception e) {
-			throw new RuntimeException("Erro recuperando conex„o com o banco", e);
+			throw new RuntimeException("Erro recuperando conex√£o com o banco", e);
 		}
 	}
 
